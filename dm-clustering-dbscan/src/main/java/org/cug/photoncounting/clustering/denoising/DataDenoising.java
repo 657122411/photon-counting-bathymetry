3f8ee023acc81e92efbe6db49c1f3f3b704442f0
taojianhao <taojianhao@foxmail.com>
2019-11-11 13:56:33 +0800

    初始化init

---
+++ dm-clustering-dbscan/src/main/java/org/cug/photoncounting/clustering/denoising/DataDenoising.java
@@ -0,0 +1,20 @@
+package org.cug.photoncounting.clustering.denoising;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cug.photoncounting.clustering.common.Point2D;
+import org.cug.photoncounting.clustering.dbscan.EpsEstimator;
+
+import java.util.List;
+
+public class DataDenoising {
+
+    private static final Log LOG = LogFactory.getLog(DataDenoising.class);
+    private final List<Point2D> allPoints = Lists.newArrayList();
+
+
+    public static void main(String[] args) {
+
+    }
+}
